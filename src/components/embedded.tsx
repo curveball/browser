ea158a73900346248ca1710f552666f1b9c1a90b
Evert Pot <me@evertpot.com>
2022-04-07 00:51:57 -0400

    Use the original JSON, pre-processing

--- src/components/embedded.tsx
+++ src/components/embedded.tsx
@@ -9,7 +9,12 @@ export function Embedded(props: PageProps) {
 
   return <>
     <h2>Embedded</h2>
-    { embeds.map( embeddedState => <Embed resourceState={embeddedState} options={props.options} csrfToken={props.csrfToken} />) }
+    { embeds.map( embeddedState => <Embed
+      resourceState={embeddedState}
+      options={props.options}
+      csrfToken={props.csrfToken}
+      originalBody={embeddedState.serializeBody() as string}
+    />) }
   </>;
 
 }
--- src/components/hal-body.tsx
+++ src/components/hal-body.tsx
@@ -5,21 +5,11 @@ import JsonViewer from './json-viewer';
 
 export function HalBody(props: PageProps) {
 
-  let tmpBody:string;
-
-  if (props.options.fullBody) {
-    tmpBody = props.resourceState.serializeBody() as string;
-  } else {
-    tmpBody = JSON.stringify(Object.assign({}, props.resourceState.data));
-  }
-
-  if (Object.keys(tmpBody).length === 0) {
-    return <></>;
-  }
+  const body = props.originalBody;
 
   return <>
     <h2>Contents</h2>
-    <code className="hljs"><JsonViewer data={tmpBody} /></code>
+    <code className="hljs"><JsonViewer data={body} /></code>
   </>;
 
 }
