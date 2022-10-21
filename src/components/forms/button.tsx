ad3c9089b43c524ceb44aac4f18d8fb16f23b9b3
Evert Pot <me@evertpot.com>
2022-10-21 18:14:00 -0400

    Docs

--- src/components/forms/button.tsx
+++ src/components/forms/button.tsx
@@ -1,8 +1,25 @@
 import * as React from 'react';
 
 type Props = {
+  /**
+   * The HTTP method associated with this button.
+   */
   method: string;
+
+  /**
+   * The label of the button. If this is not provided, we'll guess it based
+   * on the HTTP method and titleHint.
+   */
   title?: string|null;
+
+  /**
+   * If provided, we'll try to extract a good button label from this.
+   * Currently we just look for the first word in this string and if it
+   * matches a known verb we use that as the label.
+   *
+   * For example the titleHint might be 'Edit This User', in which case
+   * the label for the button just becomes 'Edit'.
+   */
   titleHint?: string;
 }
 
