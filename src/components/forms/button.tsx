5f331f1848756751133a5432f598fff0fe6463dd
Evert Pot <me@evertpot.com>
2022-10-21 18:09:35 -0400

    Guess the HAL form label.
    If a HAL Form's title property starts with a word like 'Add' or 'Edit',
    then we also guess that to be a good label for the 'submit button' for
    these forms.

--- src/components/forms/button.tsx
+++ src/components/forms/button.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 type Props = {
   method: string;
   title?: string|null;
+  titleHint?: string;
 }
 
 const methodLabel: Record<string, string> = {
@@ -12,13 +13,25 @@ const methodLabel: Record<string, string> = {
   'SEARCH' : 'Search',
 };
 
+const titleHintGuess: Record<string, string> = {
+  'add': 'Add',
+  'edit': 'Edit',
+};
 
 export function Button(props: Props) {
 
-  const label = props.title || methodLabel[props.method] || 'Submit';
+  const label = props.title || guessTitle(props.titleHint) || methodLabel[props.method] || 'Submit';
 
   return <button type="submit" className={'method-' + props.method.toLowerCase()}>
     {label}
   </button>;
+
+}
+
+function guessTitle(titleHint: string|undefined): string|undefined {
+
+  if (!titleHint) return;
+  const firstWord = titleHint.split(' ')[0].toLowerCase();
+  return titleHintGuess[firstWord];
 
 }
